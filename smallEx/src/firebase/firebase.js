e6de471ec2707f05c8497b9d0fe870a209644ef4
kiran <kiranugale239@gmail.com>
2024-04-23 09:46:51 +0530

    add firebase for getting url

---
+++ smallEx/src/firebase/firebase.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyXGEKYAug5tSJUu0s_uBmDacUb0V70Ow",
+  authDomain: "musico-9bf36.firebaseapp.com",
+  projectId: "musico-9bf36",
+  storageBucket: "musico-9bf36.appspot.com",
+  messagingSenderId: "481591878321",
+  appId: "1:481591878321:web:0595557ad5e774152cb890",
+  measurementId: "G-1Z26M6NX2C",
+};
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// Create a root reference
+const storage = getStorage();
+var data;
+export const imageHandler = async (userImg) => {
+  console.log(userImg);
+  const mountainsRef = ref(storage, `smallEx/${userImg.name}`);
+
+  // Create a reference to 'images/mountains.jpg'
+  const mountainImagesRef = ref(storage, `smallEx/${userImg}`);
+  const res = await uploadBytes(mountainsRef, userImg);
+  if (res) {
+    const url = await getDownloadURL(ref(storage, `smallEx/${userImg.name}`));
+    return url;
+  }
+};
+
+export const videoSender = async (userVideo) => {
+  const videoRef = ref(storage, `postvideo/${userVideo.name}`);
+  const myvideoRef = ref(storage, `postvideo/${userVideo}`);
+  const res = await uploadBytes(videoRef, userVideo);
+  if (res) {
+    const url = await getDownloadURL(
+      ref(storage, `postvideo/${userVideo.name}`)
+    );
+
+    return url;
+  }
+};
