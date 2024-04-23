4f8666c9a1425f2ad090dbfab01f7797549a88ae
kiran <kiranugale239@gmail.com>
2024-04-23 09:47:57 +0530

    add routes

--- smallEx/src/App.jsx
+++ smallEx/src/App.jsx
@@ -5,13 +5,29 @@ import "./App.css";
 import Topnavbar from "./Component/Navbar/Topnavbar";
 import Bottombar from "./Component/Navbar/Bottombar";
 
+import Product from "./Component/Product/Product";
+import Home from "./pages/Home/Home";
+import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
+import Singup from "./Component/Auth/Singup";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Topnavbar />
-      <Bottombar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/singup" element={<Singup />} />
+          <Route path="/" element={<Home />}>
+            <Route path="/product" />
+            <Route path="/sell" />
+            <Route path="/chat" />
+            <Route path="/myproduct" />
+            <Route path="/account" />
+          </Route>
+          <Route path="/about" element={<Topnavbar />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
