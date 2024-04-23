7a76694fb61f5068660985ea6807716879d8ca41
kiran <kiranugale239@gmail.com>
2024-04-23 09:47:32 +0530

    add pages folder

---
+++ smallEx/src/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Product from "../../Component/Product/Product";
+import { Route, Router, RouterProvider, Routes } from "react-router-dom";
+import Topnavbar from "../../Component/Navbar/Topnavbar";
+import Bottombar from "../../Component/Navbar/Bottombar";
+import Sellproduct from "../../Component/Sellproduct/Sellproduct";
+
+export default function Home() {
+  return (
+    <>
+      <div className="">
+        <Topnavbar />
+        <Bottombar />
+      </div>
+
+      <div className="">
+        <Routes>
+          <Route path="/" element={<Product />} />
+          <Route path="/chat" element={<h1>chat</h1>} />
+          <Route path="/sell" element={<Sellproduct />} />
+          <Route path="/myproduct" element={<h1>my product</h1>} />
+          <Route path="/account" element={<h1>Account</h1>} />
+        </Routes>
+      </div>
+    </>
+  );
+}
