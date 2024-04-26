694ce72740785e7f673f1bcec3a35a023b80de86
kiran <kiranugale239@gmail.com>
2024-04-26 21:26:33 +0530

    update

--- smallEx/src/Component/Product/Productcard.jsx
+++ smallEx/src/Component/Product/Productcard.jsx
@@ -1,63 +1,60 @@
 import React, { useState } from "react";
 import Seeproduct from "./Seeproduct";
 import { useNavigate } from "react-router";
+import { NavLink, Route, Router, Routes } from "react-router-dom";
 
 export default function Productcard(props) {
   return (
     <>
-      <Card
-        index={props.i}
-        setCurrentPage={props.setCurrentPage}
-        setPostsPerPage={props.setPostsPerPage}
-      />
+      <Card index={props.i} setIndex={props.setIndex} data={props.data} />
     </>
   );
 }
 
 export const Card = (props) => {
   const [page, setPage] = useState(true);
+  const navigate = useNavigate();
   return (
     <>
-      {page ? (
-        <>
-          <div
-            className="card"
+      <NavLink to="/seeproduct" style={{ textDecoration: "none" }}>
+        <div
+          className="card shadow-lg p-3 mb-5 bg-body-tertiary rounded"
+          style={{
+            width: "20rem",
+            position: "relative",
+            margin: "1rem 1rem",
+            height: "55vh",
+            overflow: "hidden",
+          }}
+          onClick={() => {
+            props.setIndex(props.index);
+          }}
+        >
+          <img
+            src={props.data.product_img1}
+            className="card-img-top"
+            alt="..."
             style={{
-              width: "18rem",
-              position: "relative",
-              margin: "1rem 1rem",
-              height: "50vh",
-              overflow: "hidden",
+              height: "25vh",
+              width: "auto",
+              margin: " 10px auto",
             }}
-            onClick={() => {
-              setPage(false);
-            }}
-          >
-            <img
-              src="programmer.jpg"
-              className="card-img-top"
-              alt="..."
-              style={{ height: "25vh", width: "15rem", margin: " 10px auto" }}
-            />
-            <div className="card-body">
-              <p className="card-text h5">
-                <b>{props.index}</b>
-              </p>
+          />
 
-              <p className="text-start " style={{ fontWeight: "400" }}>
-                product description inside this box
-              </p>
-              <p className="text-start" style={{ fontWeight: "400" }}>
-                Product address
-              </p>
-            </div>
+          <div className="card-body">
+            <p className="card-text h5">
+              <b>Rs.{props.data.price}</b>
+            </p>
+            <p className="text-start " style={{ fontWeight: "400" }}>
+              {props.data.name}
+            </p>
+
+            <p className="text-start" style={{ fontWeight: "400" }}>
+              {props.data.location}
+            </p>
           </div>
-        </>
-      ) : (
-        <>
-          <Seeproduct index={props.index} />
-        </>
-      )}
+        </div>
+      </NavLink>
     </>
   );
 };
