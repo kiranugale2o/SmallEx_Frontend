ec47d489e9d568066cfa71c8d20dc4614428f749
kiran <kiranugale239@gmail.com>
2024-04-26 21:29:16 +0530

    update

--- smallEx/src/App.jsx
+++ smallEx/src/App.jsx
@@ -9,20 +9,22 @@ import Product from "./Component/Product/Product";
 import Home from "./pages/Home/Home";
 import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 import Singup from "./Component/Auth/Singup";
+import Login from "./Component/Auth/Login";
+import Map from "./Component/Map/Map";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/singup" element={<Singup />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />}>
             <Route path="/product" />
+            <Route path="/seeproduct" />
             <Route path="/sell" />
-            <Route path="/chat" />
-            <Route path="/myproduct" />
+            <Route path="/notification" />
+            <Route path="/mystore" />
             <Route path="/account" />
           </Route>
           <Route path="/about" element={<Topnavbar />} />
