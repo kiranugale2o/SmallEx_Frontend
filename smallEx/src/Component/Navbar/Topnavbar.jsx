f5ce11dd51e9b3670284300d4f76877417efbb2c
kiran <kiranugale239@gmail.com>
2024-04-26 21:25:49 +0530

    update topnavbar

--- smallEx/src/Component/Navbar/Topnavbar.jsx
+++ smallEx/src/Component/Navbar/Topnavbar.jsx
@@ -14,22 +14,14 @@ export default function Topnavbar() {
           >
             SmallEx
           </NavLink>
-          <form class="d-flex" style={{ margin: "auto 0px" }} role="searchs">
-            <input
-              class="form-control"
-              type="search"
-              placeholder="Search Products"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
           <div class="collapse navbar-collapse" id="nav-items">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item" id="icon" style={{ margin: "20px 0px" }}>
+            <ul
+              class="navbar-nav me-auto mb-2 mb-lg-0"
+              style={{ margin: "20px 50%" }}
+            >
+              <li class="nav-item" id="icon" style={{ margin: " auto" }}>
                 <NavLink
-                  to="/"
+                  to="/notification"
                   class="nav-link active"
                   aria-current="page"
                   style={{ textDecoration: "none", color: "black" }}
@@ -37,19 +29,18 @@ export default function Topnavbar() {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="36"
-                    height="24"
+                    height="30"
                     fill="currentColor"
-                    class="bi bi-chat-text"
+                    class="bi bi-bell-fill"
                     viewBox="0 0 16 16"
                   >
-                    <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
-                    <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8m0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5" />
+                    <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
                   </svg>
                 </NavLink>
               </li>
               <li class="nav-item " id="icon" style={{ margin: "20px " }}>
                 <NavLink
-                  to="notification"
+                  to="mystore"
                   class="nav-link active"
                   aria-current="page"
                   style={{ textDecoration: "none", color: "black" }}
@@ -57,13 +48,13 @@ export default function Topnavbar() {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="36"
-                    height="24"
+                    height="30"
                     fill="currentColor"
-                    class="bi bi-bell"
+                    class="bi bi-shop-window"
                     viewBox="0 0 16 16"
                   >
-                    <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6" />
-                  </svg>
+                    <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h12V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5m2 .5a.5.5 0 0 1 .5.5V13h8V9.5a.5.5 0 0 1 1 0V13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5a.5.5 0 0 1 .5-.5" />
+                  </svg>{" "}
                 </NavLink>
               </li>
               <li class="nav-item" style={{ margin: "20px 0px" }}>
@@ -101,6 +92,7 @@ export default function Topnavbar() {
             style={{
               textDecoration: "none",
               color: "white",
+
               backgroundColor: "black",
             }}
           >
