b005d3f46ef822dc8bd5e6260c963ef02ea92c04
KIRAN UGALE <141510294+kiranugale2o@users.noreply.github.com>
2024-04-27 20:32:50 +0530

    Update App.jsx

--- smallEx/src/App.jsx
+++ smallEx/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <Routes>
           <Route path="/singup" element={<Singup />} />
           <Route path="/login" element={<Login />} />
-           <Route path="/sell" element={<Login />} />
+           <Route path="/sell" element={<Sellproduct />} />
           <Route path="/" element={<Home />}>
             <Route path="/product"  />
             <Route path="/seeproduct" />
