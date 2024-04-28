13d6c7c681bf6f1b3859ccea8d7950a3cb00cffb
KIRAN UGALE <141510294+kiranugale2o@users.noreply.github.com>
2024-04-28 08:24:45 +0530

    Update app

--- smallEx/src/App.jsx
+++ smallEx/src/App.jsx
@@ -1,13 +1,19 @@
- import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Topnavbar from "./Component/Navbar/Topnavbar";
 import Bottombar from "./Component/Navbar/Bottombar";
-
+import { HashRouter } from "react-router-dom";
 import Product from "./Component/Product/Product";
 import Home from "./pages/Home/Home";
-import { BrowserRouter, Route,  Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import Singup from "./Component/Auth/Singup";
 import Login from "./Component/Auth/Login";
 import Map from "./Component/Map/Map";
@@ -15,27 +21,58 @@ import Seeproduct from "./Component/Product/Seeproduct";
 import Sellproduct from "./Component/Sellproduct/Sellproduct";
 import Notification from "./Component/Notification/Notification";
 import Myproduct from "./Component/Mystore/Myproduct";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState();
+  const [id, setId] = useState("");
+
+  useEffect(() => {
+    const auth = async () => {
+      fetch("https://small-ex-like-olx.vercel.app/user/auth", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("smallEx_access_token"),
+        },
+      }).then((res) => {
+        res.json().then((data) => {
+          if (res.status === 200) {
+            setUser(data.user);
+            setId(data.user._id);
+            localStorage.setItem("user", data.user._id);
+          } else {
+            navigate("/login");
+          }
+        });
+      });
+    };
+
+    auth();
+  }, []);
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/singup" element={<Singup />} />
-          <Route path="/login" element={<Login />} />
-          
-          <Route path="/" element={<Home />}>
-             <Route path="/sell" element={<Sellproduct />} />
-            <Route path="/product"  />
-            <Route path="/seeproduct" />
-            <Route path="/sell" />
-            <Route path="/notification"  />
-            <Route path="/mystore"/>
-            <Route path="/account" />
-          </Route>
-          <Route path="/about" element={<Topnavbar />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="">
+        <Topnavbar />
+        <Bottombar />
+      </div>
+      <Routes>
+        <Route path="/singup" element={<Singup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home id={id} />}>
+          <Route path="/seeproduct" />
+        </Route>
+        <Route
+          path="/notification"
+          element={<Notification id={id} data={user} />}
+        />
+        <Route path="/sell" element={<Sellproduct id={id} />} />
+        <Route path="/mystore" element={<Myproduct id={id} />} />
+        <Route path="/account" element={<Profile data={user} />} />
+        <Route path="/about" element={<Topnavbar />} />
+      </Routes>
     </>
   );
 }
