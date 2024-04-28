6164dcf6547a9de009e403e135aa34ae552f093a
KIRAN UGALE <141510294+kiranugale2o@users.noreply.github.com>
2024-04-28 08:25:30 +0530

    Update Home.

--- smallEx/src/pages/Home/Home.jsx
+++ smallEx/src/pages/Home/Home.jsx
@@ -1,12 +1,7 @@
 import React, { useEffect, useState } from "react";
+
 import Product from "../../Component/Product/Product";
-import { 
-  Route,
-  Router,
-  RouterProvider,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
+import { Route, Router, RouterProvider, Routes } from "react-router-dom";
 import Topnavbar from "../../Component/Navbar/Topnavbar";
 import Bottombar from "../../Component/Navbar/Bottombar";
 import Sellproduct from "../../Component/Sellproduct/Sellproduct";
@@ -16,58 +11,21 @@ import Myproduct from "../../Component/Mystore/Myproduct";
 import Profile from "../Profile/Profile";
 import Notification from "../../Component/Notification/Notification";
 
-export default function Home() {
-  const navigate = useNavigate();
-  const [user, setUser] = useState();
-  const [id, setId] = useState("");
-
-  useEffect(() => {
-    const auth = async () => {
-      fetch("https://small-ex-like-olx.vercel.app/user/auth", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: localStorage.getItem("smallEx_access_token"),
-        },
-      }).then((res) => {
-        res.json().then((data) => {
-          if (res.status === 200) {
-            setUser(data.user);
-            setId(data.user._id);
-            localStorage.setItem("user", data.user._id);
-          } else {
-            navigate("/login");
-          }
-        });
-      });
-    };
-
-    auth();
-  }, []);
+export default function Home(props) {
   return (
     <>
-      <div className="">
-        <Topnavbar />
-        <Bottombar />
-      </div>
+     
 
       <div className="" style={{ width: "100%" }}>
         <Routes>
           <Route path="/" element={<Product />}>
-          
-          <Route
-            path="/notification"
-            element={<Notification id={id} data={user} />}
-          />
-          <Route path="/sell" element={<Sellproduct id={id} />} />
-          <Route path="/mystore" element={<Myproduct id={id} />} />
-          <Route path="/account" element={<Profile data={user} />} />
-             </Route>
+            <Route path="/seeproduct"  />
+          </Route>
         </Routes>
       </div>
 
       <div className="footer">
-        <SFooter id={id} />
+        <SFooter id={props.id} />
       </div>
     </>
   );
